35a3069b8a200ebaff0d48a991321ac4bf7e48a7
Daniel Neems <DNeems@gmail.com>
2016-02-07 16:26:34 -0500

    Refactored and cleaned to code, this is the final product minus updated slides

---
+++ app/View_Functions.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+from scipy.stats.distributions import binom
+from collections import Counter
+import operator
+
+def Ranked_List(inputfile, sort_col=1, number_entires = 25, desecending = True):
+	"""creates a list from a file that is ranked and then returns the top number of elements .
+	Keyword arguments:
+	inputfile -- the location of a csv file that contains the list to be ranked 
+	number_entires -- number of entires from the list to be returned
+	descending -- should list be done in descending order
+	sort_col -- column to sort the list of list based on   
+	"""
+	Top_List = []
+	with open(inputfile,'r') as f:
+		for line in f:
+			Top_List.append(line.strip().split(',-'))
+		Top_List = sorted(Top_List,key=lambda x: float(x[sort_col]),reverse=desecending)
+	return Top_List[:number_entires]
+
+def Comment_Topic_ID(LDA_Model,Corpus_Dict,List_of_Tokens,Topic_Order = [6,10,9,5,1,2,3,7,4,8]):
+
+	"""Take's in a list of comments as tokens and returns a list of assigned topic 
+	IDs that have been transfomed have numbers that match the LDA visulization
+
+	LDA_Model -- LDA model object from Gensim 
+	Corpus_Dict -- dictonary from all comments made with gensim
+	List_of_Tokens -- List of the tokenized comments
+	Topic_Order -- List of topic order from the pylDAVis to that topic numbers in 
+	               in the graphic meet the labels
+	"""      
+	Topic_List = []
+	for T in List_of_Tokens:
+		Fit = LDA_Model.get_document_topics(Corpus_Dict.doc2bow(T))
+		tempTopic = [0,0]
+		for F in Fit:
+			if F[1]>tempTopic[1]:
+				tempTopic = F
+		Topic_List.append(tempTopic[0])
+	#Reorder the returned topics to 
+
+	Ordered_Topic_List = []
+	for T in Topic_List:
+		Ordered_Topic_List.append(Topic_Order[T])
+	return Ordered_Topic_List
+
+def Build_Global_Freq_Dict(filename):
+	"""Take's in csv file and build a dict out of it 
+	filename -- location of file 
+	"""
+	with open(filename,'r') as f:
+          Topic_Counts = {}
+          for line in f:
+            A = line.strip().split(',')
+            Topic_Counts[A[0]] = float(A[1])
+	return Topic_Counts
+      
+def Compute_Binomial_Prob(Topic_List,Global_Topic_Count):
+	
+	List_of_Topic_Dict =[]
+	Global_Keys =Global_Topic_Count.keys()
+	i = 0
+	for key,val in dict(Counter(Topic_List)).items():
+		print i,key,val
+		List_of_Topic_Dict.append({'name':key,'obs':val,'exp':int(len(Topic_List)*Global_Topic_Count[Global_Keys[i]])})
+		if  List_of_Topic_Dict[-1]['exp']>=List_of_Topic_Dict[-1]['obs']:
+			List_of_Topic_Dict[-1]['pVal']=binom.cdf(List_of_Topic_Dict[-1]['obs'], len(Topic_List),Global_Topic_Count[Global_Keys[i]] )
+		else:
+			List_of_Topic_Dict[-1]['pVal']=1-binom.cdf(List_of_Topic_Dict[-1]['obs'], len(Topic_List), Global_Topic_Count[Global_Keys[i]])
+		i +=1
+	return sorted(List_of_Topic_Dict, key=lambda x: x['obs'], reverse=True)
+
+def Style_from_pVal(List_of_Topic_Dict, colors = ['red','green'],cutoff_pval = 0.05):
+	for T in range(len(List_of_Topic_Dict)):
+		if (List_of_Topic_Dict[T]['obs'] < List_of_Topic_Dict[T]['exp']) and (List_of_Topic_Dict[T]['pVal'] <cutoff_pval):
+		  List_of_Topic_Dict[T]['name']= '<font color="'+colors[0]+'">'+List_of_Topic_Dict[T]['name']+'</font>'
+		  List_of_Topic_Dict[T]['obs']= '<font color="'+colors[0]+'">'+str(List_of_Topic_Dict[T]['obs'])+'</font>'
+		  List_of_Topic_Dict[T]['status'] = '<font color="'+colors[0]+'">'+'Depleted'+'*'+'</font>'
+		elif (List_of_Topic_Dict[T]['obs'] > List_of_Topic_Dict[T]['exp']) and (List_of_Topic_Dict[T]['pVal'] <cutoff_pval):
+		  List_of_Topic_Dict[T]['name']= '<font color="'+colors[1]+'">'+List_of_Topic_Dict[T]['name']+'</font>'
+		  List_of_Topic_Dict[T]['obs']= '<font color="'+colors[1]+'">'+str(List_of_Topic_Dict[T]['obs'])+'</font>'
+		  List_of_Topic_Dict[T]['status'] = '<font color="'+colors[1]+'">'+'Enriched'+'*'+'</font>'
+		else:
+		  List_of_Topic_Dict[T]['status'] = 'Expected'
+	return List_of_Topic_Dict
+
+def Build_WordCloud_Input(Tokens,Corpus_Dict,size_freq_mod = 75.0, num_words = 100):
+    All_Tokens = []
+    for T in Tokens:
+        All_Tokens.extend([Corpus_Dict[x[0]] for x in Corpus_Dict.doc2bow(T)])
+    Token_Freq = dict(Counter(All_Tokens))
+    Sorted_Token_Freq = sorted(Token_Freq.items(), key=operator.itemgetter(1),reverse = True)
+    Word_Cloud =[]
+    for x in Sorted_Token_Freq:
+        tempDict = {}
+        tempDict['text']=x[0]
+        tempDict['size']=int(round(x[1]*size_freq_mod/Sorted_Token_Freq[0][1]))
+        Word_Cloud.append(tempDict)
+    return Word_Cloud[:num_words]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+#import statements
 import sys
 from gensim import corpora, models, similarities
 from flask import render_template, request
@@ -12,59 +13,57 @@ import pickle
 import json 
 from collections import Counter
 import operator
-
+from scipy.stats.distributions import binom
+import View_Functions as VF
 
 @app.route('/autocomplete_drugname',methods=['GET'])
+#Build an autocomplete list for drug names
 def autocomplete():
+    #Connects to local SQL database that contains all the product names and numbers
     db = mdb.connect(user="root", host="localhost", db="epi_reddit",  charset='utf8') 
     with db: 
       cur = db.cursor()
       cur.execute("SELECT name FROM product ")
       All_Drug_Names = cur.fetchall()
       All_Drug_Names = [x[0] for x in All_Drug_Names]
-
-    search = request.args.get('autocomplete')
-    app.logger.debug(search)
+    #Returns a JSON that is used to help autocomplete drug names in drug databse 
     return jsonify(json_list=All_Drug_Names) 
 
 
 @app.route('/summaries')
+#Builds top rank list for the users to perform a more general survey of the drugs
 def summaries():
   #Top 25 by total mentions
-  Top_Mentions = []
-  with open('app/static/Data/Top25_Drug.txt','r') as f:
-    for line in f:
-      Top_Mentions.append(line.strip().split(','))
+  Top_Mentions = VF.Ranked_List('app/static/Data/Drugs_Mention_Counts_Table.csv', sort_col = 4)
   #Top 25 by non-spam comments
-  
-  Top_Good_Mentions = []
-  with open('app/static/Data/Top25_Good_Drug.txt','r') as f:
-    for line in f:
-      Top_Good_Mentions.append(line.strip().split(','))    
-
+  Top_Good_Mentions = VF.Ranked_List('app/static/Data/Drugs_Mention_Counts_Table.csv',sort_col = 1)
   #Top 25 subreeddit normalized by diving post number by log10 of SR subscribers  
-  Top_SRs = []
-  with open('app/static/Data/Top25_SRs.txt','r') as f:
-    for line in f:
-      Top_SRs.append(line.strip().split(','))    
-  return render_template("summaries.html", Top_Mentions = Top_Mentions, Top_Good_Mentions=Top_Good_Mentions, Top_SRs=Top_SRs)
+  Top_SRs = VF.Ranked_List('app/static/Data/SubReddit_Mention_Counts_Table.csv', sort_col=2)
+  return render_template("summaries.html", Top_Mentions = Top_Mentions, Top_Good_Mentions=Top_Good_Mentions,Top_SRs=Top_SRs)
+
+
+#@app.route('/index')
+#def index():
+# return render_template("index.html")
+
+@app.route('/slides')
+def slides():
+ return render_template("slides.html")
 
 @app.route('/')
-
-@app.route('/index')
-def index():
- return render_template("index.html")
-
 @app.route('/input')
-def cities_input():
+def input():
   return render_template("input.html")
 
 @app.route('/Single_Drug')
 def query_drug():
-  #pull from input fields and unput.html and store the values
+  #pull from input fields within input.html and store the values
+  
+  #Name of the product inputed
   product_name = request.args.get('Drug_ID')
+  #Number of Subreddits requested
   requested_subreddits = int(request.args.get('Num_SR'))
-  con = mdb.connect('localhost', 'root', '', 'epi_reddit',charset='utf8', init_command='SET NAMES UTF8') #host, user, password, #database
+  #Pulls the name of each the user inputed topic feilds from below the model 
   Topic = [[] for x in range(10)]
   Topic[0] = request.args.get('Topc1')
   Topic[1] = request.args.get('Topc2')
@@ -76,33 +75,37 @@ def query_drug():
   Topic[7] = request.args.get('Topc8')
   Topic[8] = request.args.get('Topc9')
   Topic[9] = request.args.get('Topc10')
-
   #Pull the posts that mention a specfic drug from SQL
+  
+  con = mdb.connect('localhost', 'root', '', 'epi_reddit',charset='utf8', init_command='SET NAMES UTF8')
   with con: 
       cur = con.cursor()
       cur.execute("""SELECT product_ID FROM product WHERE name LIKE '%s'""" % product_name )
-      Product_ID = cur.fetchall()
-      Product_Name = Product_ID[0][0]
+      Product_ID = cur.fetchone()
       sql_statment = """SELECT c.subreddit,c.body,c.spam,c.permalink,c.title 
                         FROM content c
                         INNER JOIN product_lookup pl 
                            on c.content_ID = pl.content_ID
-                        WHERE pl.product_ID = '%s'""" % Product_Name
+                        WHERE pl.product_ID = '%s'""" % Product_ID
       cur.execute(sql_statment)
       Result = cur.fetchall()
 
   #Filter out the posts that are flagged as spam 
   Good_Result = [x for x in Result if x[2] == 0]
   product_name = product_name.capitalize()
+  
+  #Check to make sure the drug is in the list or the drug is not 100% classified as spam posts 
   if len(Good_Result) != 0:
-      #Build a dictonary to descirbe the product information
-      Product_Info = {'name': product_name,'id':Product_ID[0][0],'spam': len(Result)-len(Good_Result),'other':len(Good_Result),'total':len(Result),'spam rate':'%0.002f%%' % (float(len(Result)-len(Good_Result))/len(Result)*100)}
+      #Build a dictonary to descirbe the product information that will be passed as prod-table and populate the first row ofthe Single_Drug.html document 
+      Product_Info = {'name': product_name,'id':Product_ID[0],'spam': len(Result)-len(Good_Result),'other':len(Good_Result),'total':len(Result),'spam rate':'%0.002f%%' % (float(len(Result)-len(Good_Result))/len(Result)*100)}
       
-      #Open stored tokenized comments for each drug
-      File = 'Comments for %s' % Product_ID[0][0]
+      #Open stored tokenized comments for the selected drug
+      File = 'Comments for %s' % Product_ID[0]
       with open('app/static/Tokens/drug/'+ File,'rb') as f:
         Comments = pickle.load(f)
       Tokens = [x['Tokens'] for x in Comments]
+      # Looks to see if the file has exactly 500 comments in it, because if it does that means that total topics have been cut off to the top 500 comments by score 
+      # and is only reporting those comments in the topic list and word bubble.  This then triggers a warning a string to let the user know this happened on the page that returns  
       if len(Comments) == 500:
         At_500 = 'Only Data for the Top 500 Posts are Shown'
       else:
@@ -111,51 +114,38 @@ def query_drug():
       #Load in pre-trained LDA model 
       L = models.LdaModel.load('app/static/model/All_Drug_Drugs_RM_WC_SPAM.mdl', mmap='r')
       dictionary = corpora.Dictionary.load('app/static/model/All_Drug_Drugs_RM_WC_SPAM.dict')
-      Topic_List = []
-      for T in Tokens:
-        Fit = L.get_document_topics(dictionary.doc2bow(T))
-        tempTopic = [0,0]
-        for F in Fit:
-            if F[1]>tempTopic[1]:
-                tempTopic = F
-        Topic_List.append(tempTopic[0])
-      #Reorder the returned topics to 
-      Topic_Order = [6,10,9,5,1,2,3,7,4,8]
-      Ordered_Topic_List = []
-      for T in Topic_List:
-          Ordered_Topic_List.append(Topic_Order[T])
 
-      #Build output list the matches topic names with topic sizes 
-      Topic_List = []
-      for i,c in dict(Counter(Ordered_Topic_List)).items():
-        Topic_List.append({'name':Topic[i-1],'size':c})
-      Topic_List = sorted(Topic_List, key=lambda x: x['size'], reverse = True)
-
+      #Build and stylize topic list for selected topics        
+      #Assign Topics
+      Ordered_Topic_List=VF.Comment_Topic_ID(L,dictionary,Tokens)
+      #Open global topic freqeuncy list
+      Topic_Counts = VF.Build_Global_Freq_Dict('app/static/Data/Total_Topic_Freq.txt')
+      #Calculate pVals with a binomail CDF test
+      Topic_List=VF.Compute_Binomial_Prob(Ordered_Topic_List,Topic_Counts)
+      #Change Key values to names
+      for p in Topic_List:
+        p['name'] = Topic[int(p['name'])-1]
+      #Style table for a return to the HTML based on the pValues from the previosu test
+      Topic_List=VF.Style_from_pVal(Topic_List)
 
 
       #Build Dictionary for word cloud
-      All_Tokens = []
-      for T in Tokens:
-          All_Tokens.extend([dictionary[x[0]] for x in dictionary.doc2bow(T)])
-      sorted_x = sorted(dict(Counter(All_Tokens)).items(), key=operator.itemgetter(1),reverse = True)
-      Word_Cloud =[]
-      for x in sorted_x:
-          tempDict = {}
-          tempDict['text']=x[0]
-          tempDict['size']=int(round(x[1]*75.0/sorted_x[0][1]))
-          Word_Cloud.append(tempDict)
-      
+      Word_Cloud =VF.Build_WordCloud_Input(Tokens,dictionary)
+      #SECTION ON SUBREDDIT FREQ 
 
-      subreddit_list = [x[0] for x in Good_Result]
-      subreddit_freq_List = []
-      for k,v in (dict(Counter(subreddit_list))).iteritems():
-        subreddit_freq_List.append({'name':k, 'number':v })
-      subreddit_freq_List = sorted(subreddit_freq_List, reverse=True, key=lambda k: k['number']) 
-      SR = subreddit_freq_List[:requested_subreddits]
+      #Open global SR freqeuncy list
+      Unique_Mentions_SRs_FreqDict = VF.Build_Global_Freq_Dict('app/static/Data/Total_UniqueComment_SR_Freq.txt')
+      # Grab all the subreddits from the SQL query
+      Subreddits = [x[0] for x in Good_Result]
+      SR_List=VF.Compute_Binomial_Prob(Subreddits,Unique_Mentions_SRs_FreqDict)
+      for SR in SR_List:
+        SR['url'] = SR['name']
+      SR_List=VF.Style_from_pVal(SR_List)
       return render_template("Single_Drug.html", prod_table = Product_Info, num_subreddits = requested_subreddits,
-      drug =product_name, Word_Cloud=Word_Cloud[:100],subreddits=SR, Topic_List = Topic_List, Warning=At_500) 
+      drug =product_name, Word_Cloud=Word_Cloud, Topic_List = Topic_List, Warning=At_500, subreddits=SR_List[:requested_subreddits]) 
 
   else: 
+    #Return if the drug name is not found or if it is 100% spam posts that are found
     if len(Result) == 0:
       Source = product_name + ' was not found in any Reddit comments' 
     else:
